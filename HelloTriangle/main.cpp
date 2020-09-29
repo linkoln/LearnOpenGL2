b2db948463b765ca3fd9234016af5678ae8a9818
linkoln <linlianqin@corp.netease.com>
2020-09-29 15:30:43 +0800

    Use EBO.

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -76,12 +76,14 @@ int main()
 //    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    float vertices[] =
-    {
-        -0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f
-        };
+    float vertices[] = {-0.5f, -0.5f, 0.0f,
+                        0.5f, -0.5f, 0.0f,
+                        -0.5f, 0.5f, 0.0f,
+                        0.5f, 0.5f, 0.0f
+                       };
+    unsigned int indices[] = {0, 1, 2,
+                              1, 2, 3
+                             };
 
     const std::string vertexShader = "#version 330 core\n"
                                      "layout(location = 0) in vec3 pos;\n"
@@ -96,12 +98,15 @@ int main()
                                        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
                                        "}\n\0";
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (indices), indices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof (float), (void*)0);
@@ -110,6 +115,7 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -120,7 +126,8 @@ int main()
 
         glUseProgram(program);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
